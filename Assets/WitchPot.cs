cc035d53f0722da5787b7b9e4542597ba45994cb
yuzzznk <yujin.k@snu.ac.kr>
2023-07-16 08:32:59 +0900

    인스펙터 작업 끝

--- Assets/WitchPot.cs
+++ Assets/WitchPot.cs
@@ -10,12 +10,22 @@ public class WitchPot : MonoBehaviour
     public Image oa1;
     public Image ob1;
     public Image oc1;
+
+    public Image rw1;
+    public Image ra1;
+    public Image rb1;
+    public Image rc1;
     
     public Image wa2;
     public Image wb2;
     public Image oa2;
     public Image ob2;
     public Image oc2;
+
+    public Image rw2;
+    public Image ra2;
+    public Image rb2;
+    public Image rc2;
     
     public Image wa3;
     public Image wb3;
@@ -23,11 +33,23 @@ public class WitchPot : MonoBehaviour
     public Image ob3;
     public Image oc3;
 
+    public Image rw3;
+    public Image ra3_1;
+    public Image ra3_2;
+    public Image rb3_1;
+    public Image rb3_2;
+    public Image rc3;
+
     public Image wa4;
     public Image wb4;
     public Image oa4;
     public Image ob4;
     public Image oc4;
+
+    public Image rw4;
+    public Image ra4;
+    public Image rb4;
+    public Image rc4;
 
     public Image wa5;
     public Image wb5;
@@ -35,11 +57,21 @@ public class WitchPot : MonoBehaviour
     public Image ob5;
     public Image oc5;
 
+    public Image rw5;
+    public Image ra5;
+    public Image rb5;
+    public Image rc5;
+
     public Image wa6;
     public Image wb6;
     public Image oa6;
     public Image ob6;
     public Image oc6;
+
+    public Image rw6;
+    public Image ra6;
+    public Image rb6;
+    public Image rc6;
 
     public Image wa7;
     public Image wb7;
@@ -47,11 +79,21 @@ public class WitchPot : MonoBehaviour
     public Image ob7;
     public Image oc7;
 
+    public Image rw7;
+    public Image ra7;
+    public Image rb7;
+    public Image rc7;
+
     public Image wa8;
     public Image wb8;
     public Image oa8;
     public Image ob8;
     public Image oc8;
+
+    public Image rw8;
+    public Image ra8;
+    public Image rb8;
+    public Image rc8;
 
     // Start is called before the first frame update
     void Start()
