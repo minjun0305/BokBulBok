51adb68e0f3e22cfcd2bc84bb413a53010d9f901
yuzzznk <yujin.k@snu.ac.kr>
2023-07-16 04:25:06 +0900

    아서왕의 벌크업 구현완료

---
+++ Assets/KingArthersBulkUpReset.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingArthersBulkUpReset : MonoBehaviour
+{
+    public void Retry()
+    {
+        KingArthersBulkUp.Retry();
+    }
+}
