fe809c15692c8dd7aafaa7a20b626090dfe7dde6
minjun sun(seon) <nebe.ep@snu.ac.kr>
2023-07-15 19:37:37 +0900

    Implemented game control logic
    and sample game choice of solomon

---
+++ Assets/Scripts/GameCommonData.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameCommonData : MonoBehaviour
+{
+    public string gameName;
+    public float timeLimit;
+    public List<int> itemInfo;
+    public UnityEvent initFunc;
+    public UnityEvent timeoverFunc;
+
+    public int returnValue = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Timeover()
+    {
+        timeoverFunc.Invoke();
+        this.GetComponentInParent<GameControl>().EndGameWith(returnValue);
+    }
+}
