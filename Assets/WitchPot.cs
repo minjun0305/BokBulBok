2e65f2b53dd6ba2be002ef9d4ca958589b885c8e
yuzzznk <yujin.k@snu.ac.kr>
2023-07-16 07:10:20 +0900

    레이아웃

---
+++ Assets/WitchPot.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WitchPot : MonoBehaviour
+{
+    public Image wa1;
+    public Image wb1;
+    public Image oa1;
+    public Image ob1;
+    public Image oc1;
+    
+    public Image wa2;
+    public Image wb2;
+    public Image oa2;
+    public Image ob2;
+    public Image oc2;
+    
+    public Image wa3;
+    public Image wb3;
+    public Image oa3;
+    public Image ob3;
+    public Image oc3;
+
+    public Image wa4;
+    public Image wb4;
+    public Image oa4;
+    public Image ob4;
+    public Image oc4;
+
+    public Image wa5;
+    public Image wb5;
+    public Image oa5;
+    public Image ob5;
+    public Image oc5;
+
+    public Image wa6;
+    public Image wb6;
+    public Image oa6;
+    public Image ob6;
+    public Image oc6;
+
+    public Image wa7;
+    public Image wb7;
+    public Image oa7;
+    public Image ob7;
+    public Image oc7;
+
+    public Image wa8;
+    public Image wb8;
+    public Image oa8;
+    public Image ob8;
+    public Image oc8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
