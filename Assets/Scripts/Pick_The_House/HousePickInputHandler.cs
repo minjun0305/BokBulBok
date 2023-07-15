846c800dc8d999c1d3a18f9ed85348a7303d7793
vexatone <vexatone160@gmail.com>
2023-07-16 04:37:50 +0900

    Added Pick The Piggy House game

---
+++ Assets/Scripts/Pick_The_House/HousePickInputHandler.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HousePickInputHandler : MonoBehaviour
+{
+    [SerializeField] private HousePositionContainer housePositions;
+    private HouseController _houseControl;
+    private PlayerBehaviour _playerControl;
+
+    private void Start()
+    {
+        _houseControl = GetComponentInChildren<HouseController>();
+        _playerControl = GetComponentInChildren<PlayerBehaviour>();
+    }
+    
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            for (int i = 1; i <= 3; i++)
+            {
+                Vector2 housePosition = i switch
+                {
+                    1 => new Vector2(housePositions.houseX1, housePositions.houseY),
+                    2 => new Vector2(housePositions.houseX2, housePositions.houseY),
+                    3 => new Vector2(housePositions.houseX2, housePositions.houseY),
+                    _ => Vector2.zero
+                };
+
+                if (clickPosition.x <= housePosition.x + 0.5f && clickPosition.x >= housePosition.x - 0.5f &&
+                    clickPosition.y <= housePosition.y + 0.5f && clickPosition.y >= housePosition.y - 0.5f &&
+                    !_playerControl.ChoseHouse)
+                {
+                    StartCoroutine(ProcessGameFlow(i));
+                }
+            }
+        }
+    }
+
+    private IEnumerator ProcessGameFlow(int index)
+    {
+        _playerControl.GoIntoTheHouse(index);
+        yield return new WaitForSeconds(1);
+
+        _houseControl.Blow();
+        yield return new WaitForSeconds(1);
+        
+        _houseControl.ProcessSuccessOrFailure(index);
+    }
+}
