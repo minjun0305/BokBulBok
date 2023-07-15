a7e854312dcabce1794864d0330578d6f5492d0f
minjun sun(seon) <nebe.ep@snu.ac.kr>
2023-07-15 12:52:02 +0900

    Added simple API

--- Assets/Scripts/MouseFollower.cs
+++ Assets/Scripts/MouseFollower.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MouseFollower : MonoBehaviour
 {
+
+    public Vector3 AbsoluteAnkerPosition;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class MouseFollower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+                Input.mousePosition.y, -Camera.main.transform.position.z));
+        //Quaternion rotation = Mathf.Atan2(point.x, 
+        //this.GetComponent<RectTransform>().rotation.
     }
 }
