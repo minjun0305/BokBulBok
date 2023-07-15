f35cf7ef438f61298e7cd009e93759ba35bf816c
yuzzznk <yujin.k@snu.ac.kr>
2023-07-16 05:02:20 +0900

    Update GrapeSeedRemoving.cs

--- Assets/GrapeSeedRemoving.cs
+++ Assets/GrapeSeedRemoving.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GrapeSeedRemoving : MonoBehaviour
 {
@@ -46,7 +47,7 @@ public class GrapeSeedRemoving : MonoBehaviour
     void Update()
     {
         if (state == State.Intro) {    
-            
+
 
 
 
